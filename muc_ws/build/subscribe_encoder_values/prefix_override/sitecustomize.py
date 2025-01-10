dcb272e84acf334a125f2f559a016ebf84525347
Akshay <akshay.49@outlook.com>
2025-01-10 17:18:11 +0530

    Odom Linear motion working.

--- muc_ws/build/subscribe_encoder_values/prefix_override/sitecustomize.py
+++ muc_ws/build/subscribe_encoder_values/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/anirudh/MUC-Hardware/muc_ws/install/subscribe_encoder_values'
+    sys.prefix = sys.exec_prefix = '/home/muc/MUC-Hardware/muc_ws/install/subscribe_encoder_values'
